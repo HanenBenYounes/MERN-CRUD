5d1e11ba90b9700999f4b1510e5d6f963a3cd433
hanenBenyounes <hanen.benaliessalah@gmail.com>
2023-04-26 22:37:13 +0100

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,27 @@
 const express =require ("express")
-const app = express()
 
+const cors= require("cors")
+const mysql = require ("mysql")
+const app = express()
+app.use(cors())
+
+const db=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"crud"
+})
+
+app.get("/",(req,res)=>{
+    const sql="SELECT * FROM student";
+    db.query(sql, (err, data)=>{
+        if(err)return res.json ("Error");
+        return res.json("data");
+    })
+})
+app.get("/",(req,res)=>{
+    res.json("hello from backend")
+})
 app.listen(8081,()=>{
 console.log ("server started")
 })
